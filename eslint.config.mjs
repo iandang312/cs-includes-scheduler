174e745ea14a3cc51ab400f52a396263ca86b83a
iandang312 <iandang312@gmail.com>
2026-02-02 21:43:43 -0600

    ci/cd

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import js from '@eslint/js'
+import typescript from 'typescript-eslint'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import prettier from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
+
+export default [
+  { ignores: ['dist', 'node_modules'] },
+  js.configs.recommended,
+  ...typescript.configs.recommended,
+  prettier,
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': 'error',
+    },
+  },
+  
+  { ignores: ['dist', 'node_modules', 'frontend/.next'] },
+]
+
